3a0b50c4740f46ea0dbe8912a0f2aa55e03cd7d8
Peter Damgaard Marschall <15450@student.eadania.dk>
2022-09-27 20:47:45 +0200

    finetuning

--- BookListMVC/wwwroot/js/bookList.js
+++ BookListMVC/wwwroot/js/bookList.js
@@ -7,24 +7,24 @@ $(document).ready(function () {
 
 function loadDataTable() {
     dataTable = $('#DT_load').DataTable({
-        "ajax": {
-            "url": "/books/getall/",
-            "type": "GET",
-            "datatype": "json"
+        ajax: {
+            url: "/books/getall/",
+            type: "GET",
+            datatype: "json"
         },
         "columns": [
-            { "data": "name", "width": "20%" },
-            { "data": "author", "width": "20%" },
-            { "data": "isbn", "width": "20%" },
+            { data: "name", width: "20%" },
+            { data: "author", width: "20%" },
+            { data: "isbn", width: "20%" },
             {
-                "data": "id",
-                "render": function (data) {
-                    return `<div class="text-center">
-                        <a href="/Books/Upsert?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
+                data: "id", render:
+                    function (data) {
+                        return `<div class="text-center">
+                        <a href="/Books/Upsert?id=${data}" class="btn btn-success text-white" style="cursor:pointer; width:70px;">
                             Edit
                         </a>
                         &nbsp;
-                        <a class='btn btn-danger text-white' style='cursor:pointer; width:70px;'
+                        <a class="btn btn-danger text-white" style="cursor:pointer; width:70px;"
                             onclick=Delete('/Books/Delete?id='+${data})>
                             Delete
                         </a>
@@ -37,11 +37,11 @@ function loadDataTable() {
                                 <a href="/Books/Export?id=${data}&format=yaml">YAML</a>
                             </div>
                         </div>`;
-                }, "width": "40%"
+                    }, width: "40%"
             }
         ],
         "language": {
-            "emptyTable": "no data found"
+            "emptyTable": "No data found"
         },
         "width": "100%"
     });
